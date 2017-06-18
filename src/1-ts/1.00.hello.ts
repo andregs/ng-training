5e6524ed1164d7676d366b7982aa36fb6e669204
André Gomes <andregs@users.noreply.github.com>
2017-06-18 11:32:38 -0300

    setup typescript project

--- src/1-ts/1.00.hello.ts
+++ src/1-ts/1.00.hello.ts
@@ -1,1 +1,15 @@
 console.log('Hello, TypeScript!');
+
+let done: boolean = true;
+let decimal: number = 6.5;
+let fullName: string = 'André Gomes';
+
+console.log(typeof done, typeof decimal, typeof fullName);
+
+// typescript can guess types
+
+let notDone = false;
+let negative = -2;
+let another = 'John Doe';
+
+console.log(typeof notDone, typeof negative, typeof another);
